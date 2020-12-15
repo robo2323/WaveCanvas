f270fb04fdd4e906f9177fcf632b42694fe31a75
Rob <rob.harkness.dev@gmail.com>
2020-12-15 17:52:41 +0100

    initial, all the things

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+import '@qctrl/elements-css/dist/elements.min.css'
 
-function App() {
+import WaveCanvas, { Wave as WaveType } from './WaveCanvas'
+
+const targetWaveValues = { amplitude: 80, frequency: 5, phase: 0 }
+
+function Wave(): JSX.Element {
+  const [amplitude, setAmplitude] = useState(50);
+  const [frequency, setFrequency] = useState(10);
+  const [phase, setPhase] = useState(0);
+  const [animatePhase, setAnimatePhase] = useState(true);
+  const [showSummation, setShowSummation] = useState(true);
+  const [overlaySummation, setOverlaySummation] = useState(false);
+  const [phaseAnimationFactor, setPhaseAnimationFactor] = useState(0.04);
+
+  const waves: WaveType[] = useMemo(() => ([
+    { amplitude, frequency, phase, styles: { lineColor: "#680ce9" } }, // User/interactive wave
+    { ...targetWaveValues, phase: 0, styles: { lineColor: "#bd90ff", dashArray: [6, 3] } }, // Target/non interactive wave
+  ]), [amplitude, frequency, phase])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="w-full h-screen bg-white flex flex-col items-center">
+      <WaveCanvas waves={waves} waveSummationStyles={{ showSummation, overlay: overlaySummation }} animatePhase={animatePhase} phaseAnimationFactor={phaseAnimationFactor} />
+
+      <div className="flex flex-col w-1/3 space-y-4">
+        <label htmlFor="frequency" className="text-dark mr-2">
+          Frequency: {frequency}hz
+        </label>
+        <input
+          id="frequency"
+          type="range"
+          min="1"
+          max="100"
+          value={frequency}
+          onChange={(e): void => setFrequency(+e.currentTarget.value)}
+        />
+        <label htmlFor="amplitude" className="text-dark mr-2">
+          Amplitude: {amplitude}
+        </label>
+        <input
+          id="amplitude"
+          type="range"
+          min="1"
+          max="200" // TODO: set this from canvas height
+          value={amplitude}
+          onChange={(e): void => setAmplitude(+e.currentTarget.value)}
+        />
+        <label htmlFor="phase" className="text-dark mr-2">
+          Phase: {Math.round(phase * 180 / Math.PI)}°
+        </label>
+        <input
+          id="phase"
+          type="range"
+          min="0"
+          max={360 * (Math.PI / 180)}
+          step={Math.PI / 180}
+          value={phase}
+          onChange={(e): void => setPhase(+e.currentTarget.value)}
+        />
+      </div>
+
+      <div className="flex flex-col w-1/3 space-y-4 mt-3">
+        <div className="m-auto">non customer controls below, for demo only</div>
+        <label htmlFor="animationSpeed" className="text-dark">
+          (phase) animation speed:
+          <input className="ml-2 border-2 border-gray" type="number" min="0" max="0.5" step="0.01" value={phaseAnimationFactor} onChange={(e) => setPhaseAnimationFactor(+e.currentTarget.value)} />
+        </label>
+        <div className="flex justify-between">
+          <label htmlFor="animatePhase" className="text-dark">
+            Animate phase?
+        <input className="ml-2" id="animatePhase" type="checkbox" checked={animatePhase} onChange={(e) => setAnimatePhase(e.currentTarget.checked)} />
+          </label>
+          <label htmlFor="showSummation" className="text-dark">
+            Show summation of waves?
+        <input className="ml-2" id="animatePhase" type="checkbox" checked={showSummation} onChange={(e) => setShowSummation(e.currentTarget.checked)} />
+          </label>
+          <label htmlFor="overlaySummation" className="text-dark">
+            Overlay wave summation?
+        <input className="ml-2" id="overlaySummation" type="checkbox" checked={overlaySummation} onChange={(e) => setOverlaySummation(e.currentTarget.checked)} />
+          </label>
+        </div>
+      </div>
+
+    </div >
   );
 }
 
-export default App;
+export default Wave;
---
+++ src/WaveCanvas/index.tsx
@@ -0,0 +1,166 @@
+import React, { useEffect, useRef } from "react";
+import '@qctrl/elements-css/dist/elements.min.css'
+
+const DEFAULTS = {
+  waveLineWidth: 4,
+  waveLineColor: "#000",
+  axisLineWidth: 1,
+  axisLineColor: "rgba(0,0,0,0.4)",
+  summationLineWidth: 4,
+  summationLineColor: "rgba(144, 201, 247,1)",
+  phaseAnimationFactor: 0.1
+}
+
+export interface LineStyles {
+  lineColor?: string;
+  lineWidth?: number;
+  dashArray?: number[]
+};
+
+export interface Wave {
+  amplitude: number;
+  frequency: number;
+  phase: number;
+  styles?: LineStyles;
+}
+
+export interface WaveCanvasProps {
+  waves: Wave[];
+  axisStyles?: { hideAxis?: boolean; } & LineStyles;
+  phaseAnimationFactor?: number
+  waveSummationStyles?: { showSummation?: boolean; overlay?: boolean } & LineStyles;
+  animatePhase?: boolean
+}
+
+
+function WaveCanvas({ waves, axisStyles, waveSummationStyles, animatePhase = true, phaseAnimationFactor = DEFAULTS.phaseAnimationFactor }: WaveCanvasProps): JSX.Element {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect((): (() => void) => {
+    const canvas = canvasRef.current;
+
+    const context = canvas?.getContext("2d");
+
+    let requestId: number;
+
+    let animationPhase = 0
+
+    function render(): void {
+      if (!context) return;
+
+      const { width, height } = context.canvas;
+
+      context.clearRect(0, 0, width, height);
+
+      const heightOffset = waveSummationStyles?.showSummation && !waveSummationStyles.overlay ? height / 3 : height / 2;
+
+      function drawAxis(hOffset: number): void {
+        if (!context) return
+        context.beginPath();
+        context.moveTo(0, hOffset);
+        context.lineWidth = axisStyles?.lineWidth || DEFAULTS.axisLineWidth;
+        context.strokeStyle = axisStyles?.lineColor || DEFAULTS.axisLineColor;
+        context.setLineDash(axisStyles?.dashArray || []);
+        context.lineTo(width, hOffset);
+        context.stroke();
+        context.setLineDash([]);
+        context.closePath();
+      }
+
+      waves.forEach(({ amplitude, styles, frequency, phase }) => {
+
+        context.beginPath();
+
+        context.lineWidth = styles?.lineWidth || DEFAULTS.waveLineWidth;
+
+        for (let x = 0; x < width; x++) {
+          const y =
+            heightOffset +
+            amplitude *
+            Math.sin(x * 2 * Math.PI * (frequency / width) - animationPhase - (phase));
+
+          context.lineTo(x, y);
+        }
+
+        context.strokeStyle = styles?.lineColor || DEFAULTS.waveLineColor;
+        context.setLineDash(styles?.dashArray || []);
+        context.stroke();
+
+        context.closePath();
+
+      })
+
+      if (waveSummationStyles?.showSummation && waves.length === 2) {
+        const a1 = waves[0].amplitude
+        const a2 = waves[1].amplitude
+        const f1 = waves[0].frequency
+        const f2 = waves[1].frequency
+        const p1 = waves[0].phase
+        const p2 = waves[1].phase
+
+        const { dashArray, lineColor, lineWidth } = waveSummationStyles
+
+        context.beginPath();
+        context.lineWidth = lineWidth || DEFAULTS.summationLineWidth;
+
+        const offset = waveSummationStyles.overlay ? heightOffset : heightOffset * 2
+
+        for (let x = 0; x < width; x++) {
+          const y =
+            offset +
+            (a1 *
+              Math.sin(x * 2 * Math.PI * (f1 / width) - animationPhase - p1)) + (
+              a2 *
+              Math.sin(x * 2 * Math.PI * (f2 / width) - animationPhase - p2));
+
+          context.lineTo(x, y);
+        }
+
+        context.strokeStyle = lineColor || DEFAULTS.summationLineColor;
+        context.setLineDash(dashArray || []);
+        context.stroke();
+
+        context.closePath();
+
+        if (!waveSummationStyles.overlay && !axisStyles?.hideAxis) {
+          drawAxis(offset)
+       }
+      }
+
+      if (!axisStyles?.hideAxis) {
+        drawAxis(heightOffset)
+      }
+
+      if (animatePhase) {
+        animationPhase = animationPhase < width ? animationPhase + phaseAnimationFactor : 0;
+      }
+
+      requestId = requestAnimationFrame(render);
+    }
+
+    render();
+
+    return function cleanup(): void {
+      cancelAnimationFrame(requestId);
+    };
+  }, [animatePhase, axisStyles?.dashArray, axisStyles?.hideAxis, axisStyles?.lineColor, axisStyles?.lineWidth, phaseAnimationFactor, waveSummationStyles, waves]);
+
+  return (
+    <div className="flex items-center">
+      <div className="h-full flex flex-col justify-around mr-5">
+        <div className="text-dark">Interactive wave and target wave: </div>
+        <div className="text-dark">Summation of the two waves: </div>
+      </div>
+      <canvas
+        ref={canvasRef}
+        //TODO: Set the following from the parent div
+        width="800"
+        height="600"
+      />
+
+    </div>
+
+  );
+}
+
+export default WaveCanvas;
